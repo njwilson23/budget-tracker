b66153f96e470c98e4520a01e90569a2bd9c72d9
Nat Wilson <natw@fortyninemaps.com>
2017-11-25 18:34:48 -0800

    make interface basically usable

--- frontend/masonjar/src/App.js
+++ frontend/masonjar/src/App.js
@@ -2,19 +2,18 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 
-class Payment extends Component {
-    render() {
-        let pmt = this.props.payment;
-        let displayString = pmt.payer + " paid " + pmt.payee + " $" + pmt.amount;
-        return (
-            <div className="payment">
-                <p>{displayString}
-                    <button className="delete"
-                            onClick={this.props.deleteHandler}>Cancel</button>
-                </p>
+function Payment(props) {
+    let cls = (props.idx % 2 === 0) ? "payment even" : "payment odd",
+        pmt = props.payment,
+        displayString = pmt.date + ": " + pmt.payer + " paid " + pmt.payee + " $" + pmt.amount;
+    return (
+        <div className={cls}>
+            <div className="paymentString">{displayString}
+                <span className="flexSpace"></span>
             </div>
-        )
-    }
+            <div className="paymentDelete" onClick={props.deleteHandler}>{"Cancel"}</div>
+        </div>
+    )
 }
 
 class PaymentEditor extends Component {
@@ -60,9 +59,10 @@ class PaymentEditor extends Component {
     }
 
     render() {
+        let owes = "(Not sure who owes what)";
         return (
             <form onSubmit={this.handleSubmit}>
-                <label><br/>Date:<input type="text"
+                <label><br/>Date:<input type="date"
                                 value={this.state.date}
                                 onChange={this.handleDateChange}/></label>
                 <label><br/>Payer:<input type="text"
@@ -75,6 +75,7 @@ class PaymentEditor extends Component {
                                 value={this.state.amount}
                                 onChange={this.handleAmountChange}/></label>
                 <br/><input type="submit" value="Add receipt"/>
+                <br/><p>{owes}</p>
             </form>);
     }
 }
@@ -127,8 +128,13 @@ class App extends Component {
     }
 
     render() {
-        let payments = this.state.payments.map((pmt) =>
+        let payments = this.state.payments.slice()
+        payments.sort(
+            (a, b) => (a[1].date < b[1].date) ? -1 : ((a[1].date > b[1].date) ? 1 : 0)
+        );
+        let sortedPayments = payments.map((pmt, idx) =>
             <Payment key={pmt[0]}
+                     idx={idx}
                      payment={pmt[1]}
                      deleteHandler={(e) => this.deletePayment(pmt[0])}
              />
@@ -136,11 +142,17 @@ class App extends Component {
         return (
           <div className="App">
             <header className="App-header">
-              <h1 className="App-title">Spending Record</h1>
+              <h1 className="App-title">Household Spending</h1>
             </header>
-            <PaymentEditor host={this.state.host} addHandler={this.addPayment} />
-            <div>
-              {payments}
+            <div className="container">
+                <div className="editorContainer">
+                    <PaymentEditor
+                        host={this.state.host}
+                        addHandler={this.addPayment} />
+                </div>
+                <div className="paymentList">
+                  {sortedPayments}
+                </div>
             </div>
           </div>
         );
